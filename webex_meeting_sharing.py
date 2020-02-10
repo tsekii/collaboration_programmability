7e5cc58b1e5aa55fe93b15a8fa3c4fbf2341f80d
tsekii <tsekii51@gmail.com>
2020-02-10 14:01:25 +0900

    Add files via upload

---
+++ webex_meeting_sharing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+# import matplotlib.pyplot as plt
+
+#headers = ["Number", "CONFERENCE_ID","IS_SHARING"]
+df = pd.read_csv("/Users/tsekii/Downloads/analyticsAdmin.clusterB-06@ccepdemo.com_Webex_Attendee_analytics-clusterb_2019-10-09_2019-11-08.csv", true_values=["Y"],false_values=["N"])
+
+is_sharing = df[["CONFERENCE_ID", "USER_NAME","IS_SHARING"]].query('IS_SHARING == True') 
+sharing_result = tabulate(is_sharing, tablefmt="grid", headers="keys")
+#print(is_sharing) 
+print(sharing_result)
+
